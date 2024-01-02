f38452b76ff3b6570106e87cc93674c73ad3393a
Klleriston <Klleandrade@outlook.com>
2024-01-02 13:50:08 -0300

    Exibindo a lista e refatorando o codigo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  constructor() {}
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
 
   ngOnInit(): void {
   }
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Spiderman' },
+    { id: 13, name: 'Iron Man' },
+    { id: 14, name: 'Batman' },
+    { id: 15, name: 'Wonder Woman' },
+    { id: 16, name: 'Flash' },
+    { id: 17, name: 'Wolverine' },
+    { id: 18, name: 'Deadpool' },
+    { id: 19, name: 'Constantine' },
+    { id: 20, name: 'Black Panther' },
+    { id: 21, name: 'Mister Fantastic' }
+];
